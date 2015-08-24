7b65eb56edaa19149e238852064d9c17f6e8b11b
Winston Li <winston_li@hotmail.com>
2015-08-24 17:33:42 +0800

    Query kubernetes service's endpoint to construct etcd2's endpoint on master. Don't assume etcd2 existing on minions

--- image/config-and-run.sh
+++ image/config-and-run.sh
@@ -5,12 +5,17 @@
 # => try to leverage etcd's atomically creating in-order keys as broker id if new created pod
 # => use /kafka_data/myid for existing pod
 
-ETCD2_ENDPOINT="$(ip route | awk '/\<default via\>/ { print $3}'):2379"
+#ETCD2_ENDPOINT="$(ip route | awk '/\<default via\>/ { print $3}'):2379"
+KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+ETCD2_ENDPOINT="$(curl -k https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/default/endpoints/kubernetes -H "Authorization: Bearer ${KUBE_TOKEN}" | jq -r '.subsets[ ].addresses[].ip'):2379"
+
 if [ -f /kafka_data/data/myid ]; then
   BROKER_ID=`(cat /kafka_data/data/myid)`
   echo "use existing broker id: ${BROKER_ID}"
 else
-  BROKER_ID=`(curl -L http://${ETCD2_ENDPOINT}/v2/keys/${POD_NAMESPACE}/kafka -XPOST | sed 's/.*createdIndex":\([0-9]*\).*/\1/')`
+  MAX_JAVA_INT=2147483647
+  INDEX=`(curl -L http://${ETCD2_ENDPOINT}/v2/keys/${POD_NAMESPACE}/kafka -XPOST | sed 's/.*createdIndex":\([0-9]*\).*/\1/')`
+  BROKER_ID=`(expr ${INDEX} % ${MAX_JAVA_INT})`
   echo ${BROKER_ID} > /kafka_data/data/myid
   echo "create a new broker id: ${BROKER_ID}"
 fi
